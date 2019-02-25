2ea29147127c3a57433c85946c347531e8de106b
kimcnama <kimcnama@tcd.ie>
2019-02-25 21:45:05 +0000

    Can extract grades fields

--- exam_num_crop.m
+++ exam_num_crop.m
@@ -1,54 +1,108 @@
 close all
 
-%for i=1:length(exam_info)
-i=1;
-   exam_number = exam_info{i}.cropped_examnum;
+%ground truth = [520 527 1122-520 612-527]
+aspect_ratio = (1122-520)/(612-527); %width/height
+
+for j=1:length(exam_info)
+
+close all
+   exam_number = rgb2gray(exam_info{j}.cropped_examnum);
    
-   figure
-   subplot(6, 1, 1)
-   imshow(exam_number(:,:,1))
-   [im, em1] = graythresh(exam_number(:,:,1));
+   exam_number = imadjust(exam_number,stretchlim(exam_number),[]);
    
-   subplot(6, 1, 2)
-   imshow(exam_number(:,:,2))
-   [im, em2] = graythresh(exam_number(:,:,2));
-   subplot(6, 1, 3)
-   imshow(exam_number(:,:,3))
-   [im, em3] = graythresh(exam_number(:,:,3));
+   bw = edge(exam_number,'canny');
+   %figure(3);
+   %imshow(bw)
    
-   subplot(6, 1, 4)
-   imhist(exam_number(:,:,1))
-   hold on 
-   plot ([em1*256 em1*256], [0, max(imhist(exam_number(:,:,1)))])
-   hold off
-   subplot(6, 1, 5)
-   imhist(exam_number(:,:,2))
-   hold on 
-   plot ([em2*256 em2*256], [0, max(imhist(exam_number(:,:,2)))])
-   hold off
-   subplot(6, 1, 6)
-   imhist(exam_number(:,:,3))
-   hold on
-   plot ([em3*256 em3*256], [0, max(imhist(exam_number(:,:,3)))])
-   hold off
+   stats = [regionprops(bw); regionprops(not(bw))]; 
    
+   tot_pic_area = size(exam_number, 1) * size(exam_number, 2);
    
-   figure;
-   pd = fitdist(imhist(exam_number(:,:,3)));
-   %{
-   blur = imgaussfilt(exam_number,1.5);
+   stats2 = {};
+   inserted = 1;
+    hold on;  
+    for r = 1:numel(stats)
+        
+        ar = stats(r).BoundingBox(3) / stats(r).BoundingBox(4);
+        
+        if stats(r).Area < tot_pic_area * 0.9 && stats(r).Area > tot_pic_area * 0.005 ...
+                && ar > aspect_ratio*0.6 ...
+                && ar < aspect_ratio*1.2
+             
+            stats2{inserted} = stats(r);
+            inserted = inserted + 1;
+            %rectangle('Position', stats(r).BoundingBox, ...
+            %'Linewidth', 3, 'EdgeColor', 'r', 'LineStyle', '--');
+        
+        end
+    end
+    
+    if length(stats2) > 1
+        max_area = stats2{1}.Area;
+        max_area_ind = 1;
+        for r=2:length(stats2)
+            if stats2{r}.Area > max_area
+                max_area = stats2{r}.Area;
+                max_area_ind=r;
+            end
+        end
+        stats = stats2{max_area_ind};
+        stats2 = {};
+        stats2{1} = stats;
+    end
+    
+    if length(stats2) < 1
+        exam_info{j}.flag = 1;
+        fprintf('flag activated for index: %d', j);
+    else    
+        exam_info{j}.cropped_examnum2 = imcrop(exam_number, stats2{1}.BoundingBox);
+    end
+    
+end
+
+close all
+%split exam number up
+for i=1:length(exam_info)
+    if exam_info{i}.flag == 0
+    
+        exam_number = exam_info{i}.cropped_examnum2;
+        
+        BW = imbinarize(exam_number);
+        figure(1);
+        imshow(BW );
+        
+        %DETECT HOUGH LINES
+    [H,T,R] = hough(BW);
    
-   bw = imbinarize(blur);
-   
-   C = corner(bw);
-   
-   subplot(2,1,1)
-   imshow(exam_number)
-   
-   subplot(2,1,2)
-   imshow(bw);
-   hold on 
-   plot(C(:,1),C(:,2),'r*');
-   pause
-    %}
-%end
+    imshow(H,[],'XData',T,'YData',R,...
+                'InitialMagnification','fit' );
+    xlabel('\theta'), ylabel('\rho');
+    axis on, axis normal, hold on;
+    
+    P  = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
+    x = T(P(:,2)); y = R(P(:,1));
+    plot(x,y,'s','color','white');
+    
+    lines = houghlines(BW,T,R,P,'FillGap',2,'MinLength',10);
+    figure, imshow(exam_number), hold on
+    max_len = 0;
+    max_theta = 0;
+    for k = 1:length(lines)
+       xy = [lines(k).point1; lines(k).point2];
+       plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+       % Plot beginnings and ends of lines
+       plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+       plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+       % Determine the endpoints of the longest line segment
+       len = norm(lines(k).point1 - lines(k).point2);
+       if ( len > max_len)
+          max_len = len;
+          xy_long = xy;
+          max_theta = lines(k).theta;
+       end
+    end 
+    pause
+    end
+end
