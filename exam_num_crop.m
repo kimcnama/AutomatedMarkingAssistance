7c4262b2b2eec1dbc07de2be4591aa39f826dcb2
kimcnama <kimcnama@tcd.ie>
2019-03-02 19:12:35 +0000

    progress made on extracting individual digits of exam number but not complete

--- exam_num_crop.m
+++ exam_num_crop.m
@@ -1,4 +1,5 @@
 close all
+clc
 
 %ground truth = [520 527 1122-520 612-527]
 aspect_ratio = (1122-520)/(612-527); %width/height
@@ -24,8 +25,9 @@ close all
     for r = 1:numel(stats)
         
         ar = stats(r).BoundingBox(3) / stats(r).BoundingBox(4);
+        area = stats(r).BoundingBox(3) * stats(r).BoundingBox(4);
         
-        if stats(r).Area < tot_pic_area * 0.9 && stats(r).Area > tot_pic_area * 0.005 ...
+        if area < tot_pic_area * 0.9 && area > tot_pic_area * 0.005 ...
                 && ar > aspect_ratio*0.6 ...
                 && ar < aspect_ratio*1.2
              
@@ -53,7 +55,7 @@ close all
     
     if length(stats2) < 1
         exam_info{j}.flag = 1;
-        fprintf('flag activated for index: %d', j);
+        fprintf('\n flag activated for index: %d \n', j);
     else    
         exam_info{j}.cropped_examnum2 = imcrop(exam_number, stats2{1}.BoundingBox);
     end
@@ -62,38 +64,26 @@ end
 
 close all
 %split exam number up
-for i=1:length(exam_info)
-    if exam_info{i}.flag == 0
+for i=1:length(exam_info) 
+%i=3;
+
+    %if exam_info{i}.flag == 0
     
         exam_number = exam_info{i}.cropped_examnum2;
         
-        BW = imbinarize(exam_number);
-        figure(1);
-        imshow(BW );
+        BW = ~imbinarize(exam_number);
         
         %DETECT HOUGH LINES
     [H,T,R] = hough(BW);
-   
-    imshow(H,[],'XData',T,'YData',R,...
-                'InitialMagnification','fit' );
-    xlabel('\theta'), ylabel('\rho');
-    axis on, axis normal, hold on;
     
     P  = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
     x = T(P(:,2)); y = R(P(:,1));
-    plot(x,y,'s','color','white');
     
     lines = houghlines(BW,T,R,P,'FillGap',2,'MinLength',10);
-    figure, imshow(exam_number), hold on
     max_len = 0;
     max_theta = 0;
     for k = 1:length(lines)
        xy = [lines(k).point1; lines(k).point2];
-       plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-
-       % Plot beginnings and ends of lines
-       plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-       plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
 
        % Determine the endpoints of the longest line segment
        len = norm(lines(k).point1 - lines(k).point2);
@@ -103,6 +93,103 @@ for i=1:length(exam_info)
           max_theta = lines(k).theta;
        end
     end 
-    pause
+    
+    gray_exam_number = imrotate(exam_number, abs(max_theta) -90 ,'bilinear', 'loose');
+    exam_number = imrotate(BW, abs(max_theta) -90 ,'bilinear', 'loose');
+    
+    stats = [regionprops(exam_number); regionprops(not(exam_number))];
+    % show the image and draw the detected rectangles on it
+    
+    [avoid_rects_i, stats] = largest_two_areas(stats);
+    
+    stats2={};
+    inserted = 1;
+    tot_pic_area = size(exam_number, 1)*size(exam_number, 2);
+    
+    %x_diff/y_diff = 116/66
+    aspect_ratio = 1.758;
+    
+    for r=1:length(stats)
+        if stats(r).Area < 0.9*tot_pic_area
+            if ~(r == avoid_rects_i(1) || r == avoid_rects_i(2))
+                ar = stats(r).BoundingBox(3)/stats(r).BoundingBox(4);
+                if ar < 1.1*aspect_ratio && ar > 0.7*aspect_ratio
+                    stats2{inserted} = stats(r);
+                    inserted = inserted + 1;
+                end
+            end
+        end
+    end
+    
+    %remove rects that are completely inside other rects
+    stats={};
+    inserted = 1;
+    
+    for r=1:length(stats2)
+        in_another_rect = false;
+       for rr=1:length(stats2)
+          if r ~= rr
+              if rectA_contained_in_rectB(stats2{r}.BoundingBox, stats2{rr}.BoundingBox) == true
+                  in_another_rect = true;
+              end
+          end
+       end
+       if in_another_rect == false
+           stats{inserted} = stats2{r};
+           inserted = inserted + 1;
+       else
+           disp('Rect lay in another rect so ignored')
+       end
+    end
+    
+    imshow(exam_number); 
+    hold on;
+
+    for r = 1:length(stats)        
+            rectangle('Position', stats{r}.BoundingBox, ...
+            'Linewidth', 3, 'EdgeColor', 'r', 'LineStyle', '--');
+    end 
+   
+   pause
+   close all
+   
+   if length(stats) < 5
+      exam_info{i}.flag = 1;
+      fprintf('Flag turned on for index: %d \n', i);
+   else
+       
+   end
+   
+   %end
+end
+
+function [vector, stats] = largest_two_areas(stats)
+    
+    max_area = [1;2];
+    vector = [0;0];
+    
+    for r = 1:numel(stats)
+        area = stats(r).BoundingBox(3)*stats(r).BoundingBox(4);
+        stats(r).Area = area;
+        if area > min(max_area)  
+            
+            ind = find(max_area==min(max_area));
+            max_area(ind) = stats(r).Area;
+            vector(ind) = r;
+        end
+    end
+
+end
+ 
+function [bool] = rectA_contained_in_rectB(rectA, rectB)
+    bool = false;
+    
+    %check x's
+    if rectA(1) > rectB(1) && (rectA(1)+rectA(3)) < (rectB(1)+rectB(3))
+        if rectA(2) > rectB(2) && (rectA(2)+rectA(4)) < (rectB(2)+rectB(4))
+            bool = true;
+        end
     end
 end
+
+
--- junk.m
+++ junk.m
@@ -1,24 +1,12 @@
-min_match_thresh_page_turn = 5;
+rectA_contained_in_rectB(stats2{4}.BoundingBox, stats2{3}.BoundingBox)
 
-frames_to_extract = [];
-candidate_frames = [];
-
-for i=1:length(sift_matches(1, :))
-
-    if sift_matches(2, i) >= min_match_thresh_page_turn
-        candidate_frames = [candidate_frames sift_matches(:, i)];
-    else
-        if length(candidate_frames) > 0
-            len = length(candidate_frames(1, :));
-            max_matches = -1;
-            for j=1:len
-                if candidate_frames(2, len-j+1) > max_matches
-                    best_frame = candidate_frames(:, len-j+1);
-                    max_matches = candidate_frames(2, len-j+1);
-                end
-            end
-            frames_to_extract = [frames_to_extract best_frame];
-            candidate_frames = [];  
+function [bool] = rectA_contained_in_rectB(rectA, rectB)
+    bool = false;
+    
+    %check x's
+    if rectA(1) > rectB(1) && (rectA(1)+rectA(3)) < (rectB(1)+rectB(3))
+        if rectA(2) > rectB(2) && (rectA(2)+rectA(4)) < (rectB(2)+rectB(4))
+            bool = true;
         end
     end
 end
